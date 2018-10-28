f75871ff7f0ab3a4f64366b2f11967d7b4c19fb5
simons <kasimons58@gmail.com>
2018-10-28 20:38:31 +0800

    commit gitDemo

--- gitdemo/src/com/itheima/TestPrint.java
+++ gitdemo/src/com/itheima/TestPrint.java
@@ -4,12 +4,12 @@ public class TestPrint {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("world");
-        count();
+        count(5);
     }
 
-    public static void count(){
+    public static void count(int a) {
         int i = 10;
         i += 20;
-        System.out.println(i);
+        System.out.println(i + a);
     }
 }
